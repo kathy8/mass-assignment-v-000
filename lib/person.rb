5ebe17b85e22986edb92821665f40931c84a11a6
Katherine Gruenberg <katherine.gruenberg@gmail.com>
2018-01-24 18:48:03 +0000

    automatically backed up by learn

--- lib/person.rb
+++ lib/person.rb
@@ -1,2 +1,10 @@
 class Person
-end 
+  attr_accessor :name, :birthday, :hair_color, :eye_color, :height,
+  :weight, :handed, :complexion, :t_shirt_size, :wrist_size,
+  :glove_size, :pant_length, :pant_width
+ end
+
+  def initialize(attributes)
+  attributes.each { |key, value| send("#{key}=", value) }
+ end
+end
